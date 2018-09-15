ec943a80b79cb04f6c58167153da9b7695fd3dc3
Byung Ho Lee <aznnobless@gmail.com>
2018-09-15 15:16:37 -0700

    Add sample binary tree

--- src/basic/TreeNode.java
+++ src/basic/TreeNode.java
@@ -5,8 +5,28 @@ public class TreeNode
     int val;
     TreeNode left;
     TreeNode right;
+
     public TreeNode(int x)
     {
-        val = x;
+        this(x, null, null);
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static TreeNode getBasicSampleTree()
+    {
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3, node6, node7);
+        TreeNode node2 = new TreeNode(2, node4, node5);
+        TreeNode node1 = new TreeNode(1, node2, node3);
+
+        return node1;
     }
 }
